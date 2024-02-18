57e06a85ff9af8264ceb331196c1b58464d2fa34
DhruvVasisht <vasisht.dhruv7@gmail.com>
2024-02-18 23:14:48 +0530

    Day 24

--- JavaScript/Lecture 1/script.js
+++ JavaScript/Lecture 1/script.js
@@ -8,4 +8,14 @@ hi();
 const c =10;
 
 var arr=[1,2,3,4,5,6];
-var d=arr;
+arr.forEach(function(val){
+    console.log(val+1);
+})
+
+
+var me={
+    name: "Dhruv",
+    age:21,
+    contact: 9818084104
+};
+
