4bbc465a01b3fef88e17a338de68c7399be0d3a7
DhruvVasisht <vasisht.dhruv7@gmail.com>
2024-03-01 23:18:19 +0530

    Day 35

---
+++ JavaScript/Lecture 7/script.js
@@ -0,0 +1,9 @@
+// Question 1
+// var btn=document.querySelector("button");
+// var p=document.querySelector("p")
+// btn.addEventListener('click',function(){
+//     p.textContent="Hi Kya Haal";
+
+// });
+
+//Question 2
