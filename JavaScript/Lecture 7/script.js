58d42f75a91f138d21e2061906eeb4c98da4dd99
DhruvVasisht <vasisht.dhruv7@gmail.com>
2024-03-03 00:19:03 +0530

    Day 36

--- JavaScript/Lecture 7/script.js
+++ JavaScript/Lecture 7/script.js
@@ -8,12 +8,51 @@
 
 //Question 2
 
-var img1=document.querySelector("#one");
-var img2=document.querySelector("#two");
-document.querySelector("button")
-.addEventListener('click',function(){
-    var src1= img1.src;
-    var src2= img2.src;
-    img1.src=src2;
-    img2.src=src1;
+// var img1=document.querySelector("#one");
+// var img2=document.querySelector("#two");
+// document.querySelector("button")
+// .addEventListener('click',function(){
+//     var src1= img1.src;
+//     var src2= img2.src;
+//     img1.src=src2;
+//     img2.src=src1;
+// })
+
+//Question 3
+
+// var form=document.querySelector("form");
+// var ips=document.querySelectorAll('input[type="text"]');
+// var h3=document.querySelector("h3");
+
+// form.addEventListener('submit',function(event){
+//     event.preventDefault();
+//     for(var i=0;i<ips.length;i++){
+//         if(ips[i].value.trim()===""){
+//             h3.textContent="Error Hai Bhai";
+//             h3.style.color="red";
+//             break;
+//         }
+//     }
+// })
+ 
+//Question 4
+var add=document.querySelector("#add");
+var remove=document.querySelector("#remove");
+var ip=document.querySelector("input");
+var li; 
+var ul = document.querySelector("ul");
+
+add.addEventListener('click',function(){
+    if(ip.value.trim()===""){}
+    else{
+        li=document.createElement("li");
+        li.textContent=ip.value;
+        ul.appendChild(li);
+        ip.value='';     
+    }
+    
+})
+
+remove.addEventListener('click',function(){
+       ul.removeChild(li);
 })
