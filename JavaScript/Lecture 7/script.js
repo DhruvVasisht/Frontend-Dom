26e77e48b696fb60e8e254ebd097c1b4e30511f1
DhruvVasisht <vasisht.dhruv7@gmail.com>
2024-03-06 00:26:09 +0530

    Day 39

--- JavaScript/Lecture 7/script.js
+++ JavaScript/Lecture 7/script.js
@@ -86,45 +86,67 @@
 // })
 
 //Question 6
-var home=document.querySelector("#home");
-var about=document.querySelector("#about");
-var contact=document.querySelector("#contact");
-var hometext=document.querySelector("#hometext");
-var abouttext=document.querySelector("#abouttext");
-var contacttext=document.querySelector("#contacttext");
+// var home=document.querySelector("#home");
+// var about=document.querySelector("#about");
+// var contact=document.querySelector("#contact");
+// var hometext=document.querySelector("#hometext");
+// var abouttext=document.querySelector("#abouttext");
+// var contacttext=document.querySelector("#contacttext");
 
-hometext.style.display="block ";
-hometext.style.width="50%";
-hometext.style.margintop="20px";
+// hometext.style.display="block ";
+// hometext.style.width="50%";
+// hometext.style.margintop="20px";
 
-home.addEventListener('click',function(){
-    remove();
-    hometext.style.display="block ";
-    hometext.style.width="50%";
-    hometext.style.margintop="20px";
+// home.addEventListener('click',function(){
+//     remove();
+//     hometext.style.display="block ";
+//     hometext.style.width="50%";
+//     hometext.style.margintop="20px";
 
+// })
+
+
+// about.addEventListener('click',function(){
+//     remove();
+//     abouttext.style.display="block ";
+//     abouttext.style.width="50%";
+//     abouttext.style.margintop="20px";
+
+// })
+
+
+// contact.addEventListener('click',function(){
+//     remove();
+//     contacttext.style.display="block ";
+//     contacttext.style.width="50%";
+//     contacttext.style.margintop="20px";
+
+// })
+
+// function remove(){
+//     document.querySelectorAll("h3").forEach(function(h3){
+//         h3.style.display="none";
+//     })
+// }
+
+var count=0;
+var progress=document.querySelector("#progress");
+var h3=document.querySelector("h3");
+var start=document.querySelector("#btn1");
+var stop=document.querySelector("#btn2");
+var int;
+start.addEventListener('click',function(){
+    int=setInterval(function(){
+        if(count===100){
+           h3.style.opacity=1;
+           clearInterval(int);
+        }
+        count++;
+        progress.style.width=count+'%';
+    },50);
 })
 
 
-about.addEventListener('click',function(){
-    remove();
-    abouttext.style.display="block ";
-    abouttext.style.width="50%";
-    abouttext.style.margintop="20px";
-
+stop.addEventListener('click',function(){
+    clearInterval(int);
 })
-
-
-contact.addEventListener('click',function(){
-    remove();
-    contacttext.style.display="block ";
-    contacttext.style.width="50%";
-    contacttext.style.margintop="20px";
-
-})
-
-function remove(){
-    document.querySelectorAll("h3").forEach(function(h3){
-        h3.style.display="none";
-    })
-}
