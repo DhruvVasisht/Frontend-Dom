a68d5a51a891d39d4e9799e1ee8333eb9d55ff42
DhruvVasisht <vasisht.dhruv7@gmail.com>
2024-03-08 00:52:47 +0530

    Day 41

--- JavaScript/Lecture 7/script.js
+++ JavaScript/Lecture 7/script.js
@@ -155,20 +155,51 @@
 // })
 
 //Question 8
-var data=[
-    {name:"Dhruv",src:"https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
-    {name:"Jod",src:"https://images.unsplash.com/photo-1504257432389-52343af06ae3?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
-    {name:"Naman",src:"https://images.unsplash.com/photo-1522075469751-3a6694fb2f61?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjR8fHBvcnRyYWl0J3xlbnwwfHwwfHx8MA%3D%3D"},
-    {name:"Akshat",src:"https://plus.unsplash.com/premium_photo-1674777843203-da3ebb9fbca0?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjF8fHBvcnRyYWl0J3xlbnwwfHwwfHx8MA%3D%3D"}
-]
 
-var pers="";
-data.forEach(function(elem){
-    pers += `
-    <div class="person">
-      <div class="img">
-        <img src="https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="">
-      </div>
-      <h4>Dhruv</h4>
-    </div>`;
-});
+
+// var input=document.querySelector("input");
+// var data=[
+//     {name:"Dhruv",src:"https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
+//     {name:"Jod",src:"https://images.unsplash.com/photo-1504257432389-52343af06ae3?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
+//     {name:"Naman",src:"https://images.unsplash.com/photo-1522075469751-3a6694fb2f61?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjR8fHBvcnRyYWl0J3xlbnwwfHwwfHx8MA%3D%3D"},
+//     {name:"Akshat",src:"https://plus.unsplash.com/premium_photo-1674777843203-da3ebb9fbca0?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjF8fHBvcnRyYWl0J3xlbnwwfHwwfHx8MA%3D%3D"}
+// ]
+
+// var pers = "";
+// data.forEach(function (elem) {
+//   pers += `
+//     <div class="person">
+//       <div class="img">
+//         <img src="${elem.src}" alt="">
+//       </div>
+//       <h4>${elem.name}</h4>
+//     </div>`;
+// });
+// document.querySelector(".people").innerHTML = pers;
+
+// input.addEventListener("input", function () {
+//   var matching = data.filter(function (e) {
+//     return e.name.startsWith(input.value);
+//   });
+//   var newusers = "";
+//   matching.forEach(function (elem) {
+//     newusers += `
+//     <div class="person">
+//       <div class="img">
+//         <img src="${elem.src}" alt="">
+//       </div>
+//       <h4>${elem.name}</h4>
+//     </div>`;
+//   });
+//   document.querySelector(".people").innerHTML = newusers;
+// });
+
+
+//Question 9
+
+var textarea=document.querySelector("textarea");
+var counter=document.querySelector("#counter");
+textarea.addEventListener("input",function(){
+  counter.textContent= textarea.value.length;
+
+})
