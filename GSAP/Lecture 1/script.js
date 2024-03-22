6a79a7bd8b335aa1e50d033300274f773a123659
DhruvVasisht <vasisht.dhruv7@gmail.com>
2024-03-23 00:35:38 +0530

    Day 44

---
+++ GSAP/Lecture 1/script.js
@@ -0,0 +1,17 @@
+// // gsap.to("#box",{
+// //     x:1000,
+// //     rotate:360,
+// //     duration:2,
+// //     delay:1,
+// //     backgroundColor:"yellow"
+// // })
+
+// gsap.from("#box",{
+//     x:1000,
+//     rotate:360,
+//     duration:2,
+//     delay:1,
+//     backgroundColor:"yellow"
+// })
+
+gsap.to("#box1",)
