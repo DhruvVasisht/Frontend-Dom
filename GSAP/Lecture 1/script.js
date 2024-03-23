0f091cd5d750a1774c35be4588d2e578bda40261
DhruvVasisht <vasisht.dhruv7@gmail.com>
2024-03-23 18:14:18 +0530

    Day 45

--- GSAP/Lecture 1/script.js
+++ GSAP/Lecture 1/script.js
@@ -14,4 +14,28 @@
 //     backgroundColor:"yellow"
 // })
 
-gsap.to("#box1",)
+var tl=gsap.timeline()
+
+
+tl.to("#box1",{
+    x:1200,
+    rotate:360,
+    scale:0.5,
+    duration:2
+})
+
+
+tl.to("#box2",{
+    x:1200,
+    rotate:360,
+    scale:0.5,
+    duration:2
+})
+
+tl.to("#box3",{
+    x:1200,
+    rotate:360,
+    scale:0.5,
+    duration:2,
+})
+
