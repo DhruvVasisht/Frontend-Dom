64981ce2abf4b7d45999272a01a8c848bdb7db0b
DhruvVasisht <vasisht.dhruv7@gmail.com>
2024-03-25 19:42:19 +0530

    Day 46

---
+++ Obys Agency/script.js
@@ -0,0 +1,62 @@
+function loadingAnimation(){
+    
+var tl=gsap.timeline()
+tl.from(".line h1",{
+    y:150,
+    stagger:0.25,
+    duration:0.6,
+    delay:0.5
+
+})
+
+tl.from("#line3",{
+    y:1000,
+    stagger:0.20,
+    duration:0.6,
+})
+
+
+tl.from("#line1-part1",{
+    opacity:0,
+    onStart:function(){
+        
+var timer=document.querySelector("#line1-part1 h5")
+var count=0;
+setInterval(function(){
+    if(count<100){
+     count++;
+     timer.textContent=count
+    }
+
+},30)
+    }
+})
+
+tl.to(".line h2",{
+    opacity:1,
+    animationName:"anime"
+})
+
+tl.to("#loader",{
+    opacity:0,
+    duration:0.2,
+    delay:4    
+})
+
+tl.from("#page1",{
+    delay:0.2,
+    y:1600,
+    opacity:0,
+    duration:0.5,
+    ease:Power4
+})
+
+tl.to("#loader",{
+    display:"none"
+})
+
+
+}
+
+loadingAnimation()
+
