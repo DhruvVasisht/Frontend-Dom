418dd7af513f649f2f6ca28a68f8f2ff0d4a6ed7
DhruvVasisht <vasisht.dhruv7@gmail.com>
2024-03-26 00:05:03 +0530

    Day 46

--- Obys Agency/script.js
+++ Obys Agency/script.js
@@ -55,8 +55,32 @@ tl.to("#loader",{
     display:"none"
 })
 
+tl.from("#nav",{
+    opacity:0
+})
+
+
+tl.from(".hero h1",{
+    y:120,    
+    stagger:0.2
+})
+
+
+}
+
+function cursorAnimation(){
+    document.addEventListener("mousemove",function(dets){
+        gsap.to("#crsr",{
+            left:dets.x,
+            top:dets.y
+        })
+    
+    })
+    Shery.makeMagnet("#nav-part2 h4")
 
 }
 
 loadingAnimation()
+cursorAnimation()
+
 
